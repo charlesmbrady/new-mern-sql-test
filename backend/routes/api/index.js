cf3a1e6a9656012315cd5c6c83ac2ba4b76c4032
Charles <charlesmbrady@gmail.com>
2020-03-23 21:00:02 -0400

    fixed api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,6 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 // const sprintsRoutes = require('./sprints');
 
 // router.use('/sprints', sprintsRoutes);
 
-// module.exports = router;
+module.exports = router;
